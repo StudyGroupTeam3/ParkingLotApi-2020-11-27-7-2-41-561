ed2cd8ef37378878292b1c116b79d4f7f3c045d9
Ruirui Huang <rhuang2@slb.com>
2020-11-28 21:11:33 +0800

    feat: add delete and getbyname function in parkinglot service

--- ParkingLotApi/Entities/ParkingLotEntity.cs
+++ ParkingLotApi/Entities/ParkingLotEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using ParkingLotApi.Dtos;
@@ -19,6 +20,7 @@ namespace ParkingLotApi.Entities
             this.Location = parkingLotDto.Location;
         }
 
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Capacity { get; set; }
         public string Location { get; set; }
---
+++ ParkingLotApi/Services/ParkingLotService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ParkingLotApi.Dtos;
+using ParkingLotApi.Entities;
+using ParkingLotApi.Repository;
+
+namespace ParkingLotApi.Services
+{
+    public class ParkingLotService
+    {
+        private ParkingLotContext parkingLotContext;
+
+        public ParkingLotService(ParkingLotContext parkingLotContext)
+        {
+            this.parkingLotContext = parkingLotContext;
+        }
+
+        public async Task<ParkingLotDto> GetParkingLotByName(string name)
+        {
+            var foundParkingLot = await this.parkingLotContext.ParkingLots.FirstOrDefaultAsync(parkingLotEntity => parkingLotEntity.Name == name);
+            return new ParkingLotDto(foundParkingLot);
+        }
+
+        //public async Task<string> AddParkingLot(ParkingLotDto parkingLotDto)
+        //{
+        //    ParkingLotEntity parkingLotEntity = new ParkingLotEntity(parkingLotDto);
+
+        //    await parkingLotContext.ParkingLots.AddAsync(parkingLotEntity);
+        //    await this.parkingLotContext.SaveChangesAsync();
+        //    return parkingLotEntity.Name;
+        //}
+
+        public async Task DeleteParkingLot(string name)
+        {
+            var foundParkingLot = await parkingLotContext.ParkingLots.FirstOrDefaultAsync(parkingLotEntity => parkingLotEntity.Name == name);
+            this.parkingLotContext.ParkingLots.Remove(foundParkingLot);
+            await this.parkingLotContext.SaveChangesAsync();
+        }
+    }
+}
