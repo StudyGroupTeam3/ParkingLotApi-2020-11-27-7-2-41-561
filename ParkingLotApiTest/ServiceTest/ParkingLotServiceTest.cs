94f7f135b80195a2d631de997fcd1b53880242ca
Ruirui Huang <rhuang2@slb.com>
2020-11-28 23:03:34 +0800

    test: add Should_add_parkingLot_when_add_parkingLot_with_unique_name in parkinglotservicetest

--- ParkingLotApiTest/ServiceTest/ParkingLotServiceTest.cs
+++ ParkingLotApiTest/ServiceTest/ParkingLotServiceTest.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ParkingLotApi;
 using ParkingLotApi.Dtos;
@@ -35,6 +36,24 @@ namespace ParkingLotApiTest.ControllerTest
             await parkingLotService.DeleteParkingLot(parkingLotName);
 
             Assert.Equal(0, context.ParkingLots.Count());
+        }
+
+        [Fact]
+        public async Task Should_add_parkingLot_when_add_parkingLot_with_unique_name_via_parkingLotService()
+        {
+            var scope = Factory.Services.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+
+            ParkingLotContext context = scopedServices.GetRequiredService<ParkingLotContext>();
+            var parkingLotDto = GenerateParkingLotDto();
+
+            ParkingLotService parkingLotService = new ParkingLotService(context);
+
+            var parkingLotName = await parkingLotService.AddParkingLot(parkingLotDto);
+            var foundParkingLot = await context.ParkingLots.FirstOrDefaultAsync(parkingLotEntity => parkingLotEntity.Name == parkingLotName);
+
+            Assert.Equal(1, context.ParkingLots.Count());
+            Assert.Equal(parkingLotDto.Name, foundParkingLot.Name);
         }
 
         private static ParkingLotDto GenerateParkingLotDto()
