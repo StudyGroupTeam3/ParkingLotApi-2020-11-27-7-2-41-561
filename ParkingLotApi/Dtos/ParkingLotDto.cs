439c8897accb760adb80ae4c25ce7f9f9be7c556
Ziyuan <zxu25@slb.com>
2020-11-29 00:10:30 +0800

    feat: US1 add ParkingLotEntity and relative dto, init database migration

---
+++ ParkingLotApi/Dtos/ParkingLotDto.cs
@@ -0,0 +1,26 @@
+﻿using ParkingLotApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingLotApi.Dtos
+{
+    public class ParkingLotDto
+    {
+        public ParkingLotDto()
+        {
+        }
+
+        public ParkingLotDto(ParkingLotEntity parkingLotEntity)
+        {
+            this.Name = parkingLotEntity.Name;
+            this.Capacity = parkingLotEntity.Capacity;
+            this.Location = parkingLotEntity.Location;
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public string Location { get; set; }
+    }
+}
