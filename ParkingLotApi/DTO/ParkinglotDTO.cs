15233b50ab11e52993fd063b5d68c2cb4b329e3b
Jiankun YANG <JYang53@slb.com>
2020-11-28 19:00:45 +0800

    feat: add fake implementation to pass test for addParkinglot method in Parkinglot service

--- ParkingLotApi/DTO/ParkinglotDTO.cs
+++ ParkingLotApi/DTO/ParkinglotDTO.cs
@@ -10,5 +10,30 @@ namespace ParkingLotApi.DTO
         public string Name { get; set; }
         public int Capacity { get; set; }
         public string Location { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            return Equals((ParkinglotDTO)obj);
+        }
+
+        private bool Equals(ParkinglotDTO other)
+        {
+            return Name == other.Name && Capacity == other.Capacity && Location == other.Location;
+        }
     }
 }
