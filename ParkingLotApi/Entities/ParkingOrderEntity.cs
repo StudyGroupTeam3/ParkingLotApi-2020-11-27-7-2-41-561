21c258458b22b191a73df08ee525ef88ba3ce11b
Ziyuan <zxu25@slb.com>
2020-11-29 16:02:53 +0800

    feat: add ParkingOrderEntity and database migration

---
+++ ParkingLotApi/Entities/ParkingOrderEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingLotApi.Entities
+{
+    public class ParkingOrderEntity
+    {
+        public ParkingOrderEntity()
+        {
+        }
+
+        public ParkingOrderEntity(string nameOfParkingLot, string plateNumber)
+        {
+            this.NameOfParkingLot = nameOfParkingLot;
+            this.PlateNumber = plateNumber;
+            this.CreationTime = DateTime.Now;
+            this.OrderStatus = true;
+        }
+
+        [Key]
+        public int OrderNumber { get; set; }
+        public string NameOfParkingLot { get; set; }
+        public string PlateNumber { get; set; }
+        public DateTime CreationTime { get; set; }
+        public DateTime CloseTime { get; set; }
+        public bool OrderStatus { get; set; }
+    }
+}
---
+++ ParkingLotApi/Migrations/20201129080110_AddParkingOrders.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkingLotApi.Migrations
+{
+    public partial class AddParkingOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ParkingOrders",
+                columns: table => new
+                {
+                    OrderNumber = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NameOfParkingLot = table.Column<string>(nullable: true),
+                    PlateNumber = table.Column<string>(nullable: true),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CloseTime = table.Column<DateTime>(nullable: false),
+                    OrderStatus = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParkingOrders", x => x.OrderNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ParkingOrders");
+        }
+    }
+}
