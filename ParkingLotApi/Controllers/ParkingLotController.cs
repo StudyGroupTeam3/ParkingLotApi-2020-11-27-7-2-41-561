15999a80b7306649fccd30118736e4950fa7960f
Ying Fu <YFu3@slb.com>
2020-11-29 12:15:36 +0800

    feat: add Story1 AC1 created parkinglots and return 201 with no checks

--- ParkingLotApi/Controllers/ParkingLotController.cs
+++ ParkingLotApi/Controllers/ParkingLotController.cs
@@ -4,17 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ParkingLotApi.Dtos;
+using ParkingLotApi.Services;
 
 namespace ParkingLotApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("parkinglots")]
     public class ParkingLotController : ControllerBase
     {
-        [HttpGet]
-        public string Get()
+        private readonly ParkingLotService parkingLotService;
+
+        public ParkingLotController(ParkingLotService parkingLotService)
         {
-            return "Hello World";
+            this.parkingLotService = parkingLotService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ParkingLotDto>> Add(ParkingLotDto parkingLotDto)
+        {
+            var id = await this.parkingLotService.AddParkingLot(parkingLotDto);
+            return CreatedAtAction(nameof(GetById), new { id = id }, parkingLotDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ParkingLotDto>> GetById(int id)
+        {
+            var parkingLotDto = await this.parkingLotService.GetById(id);
+            return Ok(parkingLotDto);
         }
     }
 }
---
+++ ParkingLotApi/Services/ParkingLotService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ParkingLotApi.Dtos;
+using ParkingLotApi.Entities;
+using ParkingLotApi.Repository;
+
+namespace ParkingLotApi.Services
+{
+    public class ParkingLotService
+    {
+        private readonly ParkingLotContext parkingLotContext;
+
+        public ParkingLotService(ParkingLotContext parkingLotContext)
+        {
+            this.parkingLotContext = parkingLotContext;
+        }
+
+        public async Task<int> AddParkingLot(ParkingLotDto parkingLotDto)
+        {
+            ParkingLotEntity parkingLotEntity = new ParkingLotEntity(parkingLotDto);
+            await this.parkingLotContext.ParkingLot.AddAsync(parkingLotEntity);
+            await this.parkingLotContext.SaveChangesAsync();
+            return parkingLotEntity.Id;
+        }
+
+        public async Task<ParkingLotDto> GetById(int id)
+        {
+            var foundParkingLotEntity = await this.parkingLotContext.ParkingLot.FirstOrDefaultAsync(parkingLotEntity => parkingLotEntity.Id == id);
+            return new ParkingLotDto(foundParkingLotEntity);
+        }
+    }
+}
