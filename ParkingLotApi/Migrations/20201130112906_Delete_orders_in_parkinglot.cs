43e2752f004b7fa9895e0a499d7df97de4b8dfb0
ZFan7 <zfan7@slb.com>
2020-11-30 19:30:26 +0800

    refactor: change db migrations

---
+++ ParkingLotApi/Migrations/20201130112906_Delete_orders_in_parkinglot.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkingLotApi.Migrations
+{
+    public partial class Delete_orders_in_parkinglot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_ParkingLots_ParkingLotEntityName",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_ParkingLotEntityName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ParkingLotEntityName",
+                table: "Orders");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ParkingLotEntityName",
+                table: "Orders",
+                type: "varchar(255) CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ParkingLotEntityName",
+                table: "Orders",
+                column: "ParkingLotEntityName");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_ParkingLots_ParkingLotEntityName",
+                table: "Orders",
+                column: "ParkingLotEntityName",
+                principalTable: "ParkingLots",
+                principalColumn: "Name",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
