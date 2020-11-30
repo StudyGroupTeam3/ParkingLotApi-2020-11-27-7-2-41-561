f7bb91417469605d339c056b15718b6374f6621f
Ziyuan <zxu25@slb.com>
2020-11-30 11:02:20 +0800

    refactor: rename ParkingOrderCOntroller ParkingOrderService

--- ParkingLotApi/Startup.cs
+++ ParkingLotApi/Startup.cs
@@ -35,7 +35,7 @@ namespace ParkingLotApi
                 options.UseMySql(Configuration.GetConnectionString("Default"));
             });
             services.AddScoped<ParkingLotService>();
-            services.AddScoped<ParkService>();
+            services.AddScoped<ParkingOrderService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
